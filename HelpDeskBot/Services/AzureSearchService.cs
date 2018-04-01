cf3b8bc07231c9ad023fe3e2f17ee3f462922a25
makubass <makubass@gmail.com>
2018-04-02 01:15:26 +0900

    still editing until 34ofP51.

--- HelpDeskBot/Services/AzureSearchService.cs
+++ HelpDeskBot/Services/AzureSearchService.cs
@@ -23,5 +23,35 @@
                 return JsonConvert.DeserializeObject<SearchResult>(response);
             }
         }
+        
+        public async Task<FacetResult>FetchFacets()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                string facetQuery = $"{QueryString}facet=category";
+                string response = await httpClient.GetStringAsync(facetQuery);
+                return JsonConvert.DeserializeObject<FacetResult>(response);
+            }
+        }
+
+        public async Task<SearchResult>SearchByTitle(string title)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                string nameQuery = $"{QueryString}$filter=title eq '{title}'";
+                string response = await httpClient.GetStringAsync(nameQuery);
+                return JsonConvert.DeserializeObject<SearchResult>(response);
+            }
+        }
+
+        public async Task<SearchResult>Search(string text)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                string nameQuery = $"{QueryString}search={text}";
+                string response = await httpClient.GetStringAsync(nameQuery);
+                return JsonConvert.DeserializeObject<SearchResult>(response);
+            }
+        }
     }
 }
